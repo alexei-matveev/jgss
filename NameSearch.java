2aca1c49c4aa340bf629ff0190d6e4de96c570d4
Alexei Matveev <alexei.matveev@gmail.com>
2015-10-20 20:11:50 +0200

    AM: indent

--- NameSearch.java
+++ NameSearch.java
@@ -7,40 +7,40 @@ public class NameSearch
     {
         try
         {
-// Get the initial context
+            // Get the initial context
             InitialDirContext ctx = new InitialDirContext();
 
-// Create the search attributes - look for a surname of Tippin
+            // Create the search attributes - look for a surname of Tippin
             BasicAttributes searchAttrs = new BasicAttributes();
 
             searchAttrs.put("sn", "Tippin");
 
-// Search for items with the specified attribute starting
-// at the top of the search tree
+            // Search for items with the specified attribute starting
+            // at the top of the search tree
             NamingEnumeration objs = ctx.search(
                 "ldap://ldap.wutka.com/o=Wutka Consulting, dc=wutka, dc=com",
                 searchAttrs);
 
-// Loop through the objects returned in the search
+            // Loop through the objects returned in the search
             while (objs.hasMoreElements())
             {
-// Each item is a SearchResult object
+                // Each item is a SearchResult object
                 SearchResult match = (SearchResult)objs.nextElement();
 
-// Print out the node name
+                // Print out the node name
                 System.out.println("Found "+match.getName()+":");
 
-// Get the node's attributes
+                // Get the node's attributes
                 Attributes attrs = match.getAttributes();
                 NamingEnumeration e = attrs.getAll();
 
-// Loop through the attributes
+                // Loop through the attributes
                 while (e.hasMoreElements())
                 {
-// Get the next attribute
+                    // Get the next attribute
                     Attribute attr = (Attribute) e.nextElement();
 
-// Print out the attribute's value(s)
+                    // Print out the attribute's value(s)
                     System.out.print(attr.getID()+" = ");
                     for (int i=0; i < attr.size(); i++)
                     {
